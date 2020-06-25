d94f96502ee0b34ec06160407438b5a3efc7ad88
Jessie Shi <jess0407@users.noreply.github.com>
2020-06-25 18:33:28 +0100

    update asset names

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="flex flex-col">
-    <h2 class="text-4xl">{{ name }}</h2>
-    <p>{{ description }}</p>
-    <hr class="my-4" />
-    <p>{{ price }}</p>
+    <div class="drink-container">
+      <img :src="getImagePath(img)" :alt="img" class="m-auto w-64" />
+      <img :src="getIcingPath()" :alt="img" class="w-48 mx-auto" />
+      <img :src="getBobaPath()" :alt="img" class="w-40 mx-auto" />
+    </div>
+    <div class="mt-3">
+      <h2 class="text-4xl">{{ name }}</h2>
+      <p>{{ description }}</p>
+      <hr class="my-4" />
+      <p>{{ price }}</p>
+    </div>
   </div>
 </template>
 
@@ -14,7 +21,22 @@ export default {
     name: String,
     description: String,
     price: Number,
+    img: String
   },
+  methods: {
+    getImagePath: function(fileName) {
+      if (!fileName) {
+        return;
+      }
+      return require.context('@/assets/base', false, /\.svg$/)(`./${fileName}`);
+    },
+    getBobaPath: function() {
+      return require.context('@/assets/boba', false, /\.svg$/)(`./classic.svg`);
+    },
+    getIcingPath: function() {
+      return require.context('@/assets/icing', false, /\.svg$/)(`./coco.svg`);
+    }
+  }
 };
 </script>
 
