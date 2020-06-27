9fac8d76995ba2bb6cdd3ca7690f9b720228c928
Jessie Shi <jess0407@users.noreply.github.com>
2020-06-27 17:27:15 +0100

    drink maker

--- src/views/DrinkDetail.vue
+++ src/views/DrinkDetail.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>
-    <BubbleTeaDisplay />
+  <div class="grid grid-cols-5 gap-6">
+    <div class="drink-card" v-for="item in drinks" :key="item.id">
+      <BubbleTeaDisplay
+        :base2="getColor(item)"
+        :base3="getColor(item)"
+        base1="#eee"
+        base4="#cae"
+        :icing="colorCodes.watermelonDeep"
+        :showStraw="false"
+      >
+      </BubbleTeaDisplay>
+      <img :src="getImagePath(item.productImageFile)" :alt="img" />
+      <div class="text-sm font-mono font-semibold">{{ item.name }}</div>
+      <div class="text-sm font-mono font-semibold">{{ item.color }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 import BubbleTeaDisplay from '../components/bubble-tea-display';
+import httpService from '../services/http-service';
+import colorCodes from './dict';
 export default {
   components: {
     BubbleTeaDisplay
+  },
+  data() {
+    return {
+      drinks: [],
+      colorCodes
+    };
+  },
+  async created() {
+    const data = await httpService.get('teas/');
+    this.drinks = data;
+  },
+  methods: {
+    getImagePath: function(fileName) {
+      if (!fileName) {
+        return;
+      }
+      return require.context('@/assets/full', false, /\.svg$/)(`./${fileName}`);
+    },
+    getColor(drink) {
+      return colorCodes[drink.color[0]];
+    }
   }
 };
 </script>
---
+++ src/views/dict.js
@@ -0,0 +1,41 @@
+export default {
+  caramel: '#fcb737',
+  chocolate: '#452100',
+  coconut: '#ffe',
+  passionfruit: '#ffc276',
+  strawberry: '#ff84a5',
+  classic: '#e1b18c',
+  cheese: '#fcb737',
+  lychee: '#ffc4b5',
+  watermelon: '#fe6b46',
+  watermelonDeep: '#ff2010',
+  Jasmine: '#FFF8DC',
+  matcha: '#c3ce42',
+  deepMatch: '#757e0c'
+};
+// <div>
+//   <button
+//     @click="showLid = !showLid"
+//     class="px-3 py-1 text-sm font-semibold ml-2"
+//   >
+//     lid
+//   </button>
+//   <button
+//     @click="showStraw = !showStraw"
+//     class="px-3 py-1 text-sm font-semibold ml-2"
+//   >
+//     straw
+//   </button>
+//   <button
+//     @click="showIcing = !showIcing"
+//     class="px-3 py-1 text-sm font-semibold ml-2"
+//   >
+//     icing
+//   </button>
+//   <button
+//     @click="showBoba = !showBoba"
+//     class="px-3 py-1 text-sm font-semibold ml-2"
+//   >
+//     boba
+//   </button>
+// </div>
